2c3791c6edaa85ecbdc204cfc1c0c312fbf55ce9
ivchenkodima <ivchenkodima@gmail.com>
2016-08-03 01:28:21 +0300

    logic and error page

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,24 @@
 
-var App = angular.module('app', [
-    'ui.router',
-    'ui.mask'
-    
-]);
+var myApp = angular.module('myApp', ['ui.router']);
 
 
+myApp.config(['$stateProvider','$urlRouterProvider',
+    function($stateProvider,$urlRouterProvider){
+
+        $stateProvider.state('what',{
+            url:'/what',
+            templateUrl: 'template/what.html'
+        })
+        .state('what2',{
+            url:'/what2',
+            templateUrl: 'template/what2.html'
+        })
+        .state('error',{
+            url:'/error',
+            templateUrl: 'template/error.html'
+        });
+
+
+        $urlRouterProvider.otherwise("/error");
+
+    }]);
