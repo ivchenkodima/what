55559e6b9c45c11915fd22dab97648f5fb82bffd
ivchenkodima <ivchenkodima@gmail.com>
2016-08-19 13:18:20 +0300

    fix animation

--- src/js/blockanimate.js
+++ src/js/blockanimate.js
@@ -23,9 +23,7 @@ var pages = {
 setTimeout(timeout, 500);
 
 function timeout () {
-
     $(document).ready(function () {
-
         $("img").addClass("animate-static");
         $(".link").addClass("animate-static");
         for (var item = 0; item < pages.first.length; item++) {
@@ -36,42 +34,33 @@ function timeout () {
             })(item);
         }
         $(window).bind('hashchange', function() {
-            console.log("change");
             setTimeout(function () {
                 var page = $('.main').attr('id');
-                console.log(page);
                 $("img").addClass("animate-static");
                 $(".link").addClass("animate-static");
-
                 for (var item = 0; item < pages[page].length; item++) {
-                    console.log(pages[page][item]);
                     (function (index) {
                         setTimeout(function () {
                             $("#" + pages[page][index]).addClass('animate-text');
                         }, index * 1000);
                     })(item);
                 }
-            }, 0);
+            }, 20);
         });
-        $(window).trigger("hashchange");
 
-        $('.transferLink').click(function(e) {
-            console.log('click');
-            setTimeout(function () {
-                var pageClick = $('.main').attr('id');
-                console.log(pageClick);
-                $("img").addClass("animate-static");
-                $(".link").addClass("animate-static");
-
-                for (var item = 0; item < pages[pageClick].length; item++) {
-                    console.log(pages[pageClick][item]);
-                    (function (index) {
-                        setTimeout(function () {
-                            $("#" + pages[pageClick][index]).addClass('animate-text');
-                        }, index * 1000);
-                    })(item);
-                }
-            }, 10);
-        });
     });
 }
+$(document).undelegate('.transferLink', 'click').delegate('.transferLink', 'click', function() {
+    setTimeout(function () {
+        var pageClick = $('.main').attr('id');
+        $("img").addClass("animate-static");
+        $(".link").addClass("animate-static");
+        for (var item = 0; item < pages[pageClick].length; item++) {
+            (function (index) {
+                setTimeout(function () {
+                    $("#" + pages[pageClick][index]).addClass('animate-text');
+                }, index * 1000);
+            })(item);
+        }
+    }, 200);
+});
